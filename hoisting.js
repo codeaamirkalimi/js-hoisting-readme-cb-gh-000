27337592d2c01fa7f3b54adb006483875f5c60ff
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-12 19:28:08 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -5,6 +5,7 @@ function callMe() {
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
+
 }
 
 
@@ -13,19 +14,18 @@ function crazy() {
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+
   thisIsCrazy();
-
-
 }
 
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
+
+  sayMy();
+
   function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
-  sayMy();
-
-
 }
